f634bf9b846fc7a6198f7a8d3cd0bb14403ce282
Rodrigo Petters <rodrigofmp@gmail.com>
2017-10-28 15:28:38 -0200

    Save changes

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -60,7 +60,10 @@ namespace Vidly.Controllers
         [HttpPost]
         public ActionResult Create(Customer customer)
         {
-            return View();
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
         }
 
     }
