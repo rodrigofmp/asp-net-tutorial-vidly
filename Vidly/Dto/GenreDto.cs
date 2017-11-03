f58ab355e6f111f5303d952189fee18faddd9387
Rodrigo Petters <rodrigofmp@gmail.com>
2017-11-03 12:39:10 -0200

    Applying API on movies

---
+++ Vidly/Dto/GenreDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Vidly.Dto
+{
+    public class GenreDto
+    {
+        public byte Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- Vidly/Dto/MovieDto.cs
+++ Vidly/Dto/MovieDto.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
 
 namespace Vidly.Dto
 {
@@ -15,6 +12,8 @@ namespace Vidly.Dto
         public string Name { get; set; }
 
         public byte GenreId { get; set; }
+
+        public GenreDto Genre { get; set; }
 
         public DateTime ReleaseDate { get; set; }
 
