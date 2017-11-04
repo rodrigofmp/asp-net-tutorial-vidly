2e386c348aa31531db316093ef7437296e177a9d
Rodrigo Petters <rodrigofmp@gmail.com>
2017-11-04 18:56:14 -0200

    Social login

--- Vidly/App_Start/Startup.Auth.cs
+++ Vidly/App_Start/Startup.Auth.cs
@@ -54,9 +54,9 @@ namespace Vidly
             //   consumerKey: "",
             //   consumerSecret: "");
 
-            //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
+            app.UseFacebookAuthentication(
+               appId: "",
+               appSecret: "");
 
             //app.UseGoogleAuthentication(
             //    clientId: "",
