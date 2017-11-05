ebd532d61b2d31dfe338c4f2eba741e33c359219
Rodrigo Petters <rodrigofmp@gmail.com>
2017-11-05 00:14:59 -0200

    Some optimizations

--- Vidly/Controllers/HomeController.cs
+++ Vidly/Controllers/HomeController.cs
@@ -1,10 +1,15 @@
 ﻿using System.Web.Mvc;
+using System.Web.UI;
 
 namespace Vidly.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        // Enable cache
+        // [OutputCache(Duration = 50, Location = OutputCacheLocation.Server, VaryByParam = "*")]
+        // Disable cache
+        // [OutputCache(Duration = 0, VaryByParam = "*", NoStore = true)]
         public ActionResult Index()
         {
             return View();
